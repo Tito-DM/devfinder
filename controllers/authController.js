bcf1883a330c9f6907f0a2df7ce35557ebef039f
pyong <muandatito@gmail.com>
2021-08-28 23:13:53 +0100

    add login function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 const index = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
@@ -9,6 +12,46 @@ const index = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+  
+    if (!user) {
+      res.status(400).json({ erros: [{ msg: "invalid credentials" }] });
+    }
+
+    //comparar password
+    const isMatch = await bcrypt.compare(password, user.password);
+   
+    if (!isMatch) {
+      return res.status(400).json({ erros: [{ msg: "invalid credentials" }] });
+    }
+    //set payload for token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    //create token
+    jwt.sign(
+      payload,
+      process.env.JWTSECRETKEY,
+      { expiresIn: 36000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+};
+
 module.exports = {
   index,
+  login,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 const authController = require("../controllers/authController");
-const auth = require("../middleware/auth")
+const auth = require("../middleware/auth");
+const { check } = require("express-validator");
 //Get all users
 //acess public
 router.get("/", auth, authController.index);
+router.post(
+  "/login",
+  [
+    check("email", "please enter a valid email").isEmail(),
+    check("password", "password is required").exists(),
+  ],
+  authController.login
+);
 
 module.exports = router;
