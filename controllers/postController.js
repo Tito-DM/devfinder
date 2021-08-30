9174f269de9248341eacdf2a5f3653e89a6fcee8
pyong <muandatito@gmail.com>
2021-08-30 13:22:58 +0100

    create post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,43 @@
+const Post = require("../models/Post");
+const { validationResult } = require("express-validator");
+const User = require("../models/User");
 
-const index = (req,res)=>{
-    res.send("post route")
-}
+const index = (req, res) => {
+  res.send("post route");
+};
 
+const create = async (req, res) => {
+  const erros = validationResult(req);
+  //check erros
+  if (!erros.isEmpty()) {
+    return res.status(400).json({ erros: erros.array() });
+  }
+
+  try {
+    //get user
+    const user = await User.findById(req.user.id).select("-password");
+
+    //create post object
+    const newPost = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id,
+    };
+
+    //save to db
+    const post = await new Post(newPost);
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+};
 
 module.exports = {
-    index
-}
+  index,
+  create,
+};
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const postController = require("../controllers/postController");
+const auth = require("../middleware/auth");
+const {check} = require("express-validator")
 
-//Get all users
-//acess public
-router.get("/", postController.index);
+router.post("/",[auth, [check("text", "text is required").not().isEmpty()]] ,postController.create);
 
 module.exports = router;
